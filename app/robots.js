83c6093177e10c1db63099554484b0f28729579f
Matthew Willie <r3dk1ngs@gmail.com>
2025-03-24 15:53:46 -0600

    Initial commit

---
+++ app/robots.js
@@ -0,0 +1,9 @@
+export default function robots() {
+    return {
+      rules: {
+        userAgent: '*',
+        allow: '/',
+      },
+      sitemap: 'https://jackpotai.app/sitemap.xml',
+    }
+  }
